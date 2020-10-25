d1366f715c926303189549204bd70feb8b3bf559
Vincent DOUROUX <vincent.douroux@epitech.eu>
2020-10-25 20:30:54 +0100

    [PURIF] Removing trash in code

--- epicture/lib/pages/ConnectImgur.dart
+++ epicture/lib/pages/ConnectImgur.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-//import 'package:flutter/foundation.dart';
 
 import 'package:flutter_web_auth/flutter_web_auth.dart';
 import 'package:shared_preferences/shared_preferences.dart';
